10e3bac1693d95a7a80c5d865c430227c71486d2
Viacheslav Katsuba <v.katsuba.dev@gmail.com>
2019-09-19 18:39:56 +0300

    Adding Erlang Problem 11

--- src/p11.erl
+++ src/p11.erl
@@ -8,8 +8,8 @@
 %% @doc
 %% P11 (*) Modified run-length encoding.
 %%    Modify the result of problem P10 in such a way that if an element has no duplicates it is simply copied into the result list. Only elements with duplicates are transferred as (N E) lists.
-%% > p11:run([a,b,b,b,b,c,d,e,e,e,e,f]).
-%% [a, {4,b}, c, d, {4, e}, f]
+%% > p11:run([a,a,a,a,b,c,c,a,a,d,e,e,e,e]).
+%% [{4, a}, b, {2, c}, {2, a}, d, {4, e}]
 %% @end
 %% -------------------------------------------------------------------
 -spec run(L :: lists:list()) -> Result :: lists:list() | {error, badarg}.
